79d5cc25aa0c59ac073f417459d9820a52007778
Andrewl7127 <33340487+Andrewl7127@users.noreply.github.com>
2021-04-10 01:39:32 -0700

    Update app5.py

--- app5.py
+++ app5.py
@@ -2,15 +2,6 @@ import streamlit as st
 
 def app():
 
-    st.write('# Random Forest Models')
-
-    st.write('## Base Model')
-    st.image("https://blueprintdata2020.s3-us-west-1.amazonaws.com/default.PNG", use_column_width = True, output_format = 'JPEG')
-
-    st.write('## Best RandomSearchCV Model')
-    st.image("https://blueprintdata2020.s3-us-west-1.amazonaws.com/random.PNG", use_column_width = True, output_format = 'JPEG')
-
-    st.write('## Best GridSearchCV Model')
-    st.image("https://blueprintdata2020.s3-us-west-1.amazonaws.com/grid.PNG", use_column_width = True, output_format = 'JPEG')
+    st.write('# Temporary')
 
     
